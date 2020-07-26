2a2687cf80e9e93d307843ce3f8e28e02ac53b43
your_name <your_email@example.com>
2020-07-26 21:52:02 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+
+const  main = document.getElementById('main');
+main.remove ()
+
+let newHeader = document.createElement('H1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Ola is the champion'
